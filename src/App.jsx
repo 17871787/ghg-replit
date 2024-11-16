d5605ac79efabf8353fed3cd5b117fd525e9edac
17871787 <jtowers349@gmail.com>
2024-11-16 14:55:02 +0000

    cursos made main component smaller

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import GreenhouseGasAnalysisTool from './GreenhouseGasAnalysisTool'
-import './App.css'
+import React from 'react';
+import { GreenhouseGasAnalysisTool } from './GreenhouseGasAnalysisTool';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <GreenhouseGasAnalysisTool />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
